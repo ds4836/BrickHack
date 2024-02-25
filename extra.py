46528a500d13b2d8c82b6396843b40181d06e99b
ds4836 <ds4836@g.rit.edu>
2024-02-24 21:48:44 -0500

    extra

--- extra.py
+++ extra.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 # Define the base URL for the Kintone API
 base_url = "https://jlukasmay.kintone.com"
@@ -29,6 +30,11 @@ try:
     if response.status_code == 200:
         # Extract data from response JSON
         data = response.json()
+        question = data.get('records')[0].get('question').get('value')
+        option1 = data.get('records')[0].get('option1').get('value')
         print("Retrieved data:", data)
+        print()
+        print(question)
+        print(option1)
 except requests.RequestException as e:
     print("Error creating record:", e)
